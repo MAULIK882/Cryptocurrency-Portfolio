4ccfb9500165bcacaef7a16add83400099547954
Maulik Panchal <maulik882@gmail.com>
2022-03-15 12:42:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function onOpen() {
   sheet.addMenu("Cryptos Tools", entries);
 };
 
-function coinmarketcap() {
-var url = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=400";
-var response = UrlFetchApp.fetch(url);
-var text = response.getContentText();
-var obj_array = JSON.parse(text);
-return obj_array;
-}
+//function coinmarketcap() {
+//var url = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=400";
+//var response = UrlFetchApp.fetch(url);
+//var text = response.getContentText();
+//var obj_array = JSON.parse(text);
+//return obj_array;
+//}
 
 function searchcoin(symbol, myArray) {
   var err= {symbol: symbol, name: "???????", rank: "-", market_cap_eur: "0",price_btc: "0",price_eur: "0",price_usd: "0",percent_change_1h: "0",percent_change_24h: "0",percent_change_7d: "0"}
